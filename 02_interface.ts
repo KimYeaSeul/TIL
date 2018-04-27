c99303fc01e4287458a226fffce73f8226d2b276
kimyeaseul <adgjl3151@hanmail.net>
2018-04-27 14:55:17 +0900

    finish interface

--- 02_interface.ts
+++ 02_interface.ts
@@ -100,4 +100,170 @@ interface StringArray{
 let myArray:StringArray;
 myArray = ["Bob", "Fred"];
 let myStr:string = myArray[0];
-//contunue...
+//어렵다.!
+
+class Animal{
+    name:string;
+}
+
+class Dog extends Animal{
+    breed:string;
+}
+
+interface NotOkay{
+    // [x:number]: Animal; //오류.. 왜?ㅠㅠ
+    [x:string]: Dog; 
+}
+
+interface NumberDictionary{
+    [index:string]:number;
+    length:number;
+    // name:string;//string 형식의 name속성을 문자열 index형식 number에 할당 불가
+}
+
+//인덱스에 할당되지 않도록 인덱스 시그니처를 읽기 전용으로 만든다.
+interface ReadonlyStringArray{
+    readonly [index:number]: string;
+}
+
+let myArray2:ReadonlyStringArray=["Alice", "Bob"];
+// myArray2[2] = "Mallory"; //인덱스 시그니처가 읽기 전용이므로 설정 불가
+
+/* 클래스 타입(Class Types) 
+인터페이스 구현(Implementing an interface)
+이것도 뭔소리다냐..
+*/
+
+interface ClockInterface{
+    currentTime:Date;
+    setTime(d:Date); //인터페이스 메서드
+}
+
+class Clock implements ClockInterface{
+    currentTime: Date;
+    setTime(d:Date){
+        this.currentTime = d;
+    }
+    constructor(h:number,m:number){}
+}
+
+
+/* 클래스의 스태틱과 인스턴스의 차이점 
+class : 스태틱 측면의 타입과 인스턴스 측면의 타입으로 인터페이스 만들고
+이 인터페이스를 구현하는 클래스를 생성하려고 하면 오류가 발생할 수 있다.
+왜냐하면 클래스가 인터페이스를 구현할 때 클래스의 인스턴스 측면만 검사되기 때문입니다.
+생성자는 정적인 측면이기 때문에 이 검사에 포함되지 않습니다.
+*/
+
+// interface ClockConstructor{
+//     new (hour:number, minute:number);
+// }
+
+// class Clock2 implements ClockConstructor{
+//     currentTime: Date;
+//     constructor(h:number, m:number){}
+// }
+
+/* 대신 클래스의 정적인 측면에서 직접 작업해야 합니다. wow어렵당*/
+interface ClockConstructor2{
+    new (hour:number, minute:number): ClockInterface2;
+}
+
+interface ClockInterface2{
+    tick();
+}
+
+function createClock( ctor: ClockConstructor2, hour: number, minute: number):ClockInterface2{
+    return new ctor(hour, minute);
+}
+
+class DigitalClock implements ClockInterface2{
+    constructor(h:number, m:number){}
+    tick() {
+        console.log("beep beep");
+    }
+}
+class AnalogClock implements ClockInterface2{
+    constructor(h:number, m:number){}
+    tick(){
+        console.log("tick tock");
+    }
+}
+
+let digital = createClock(DigitalClock, 12,17);
+let analog = createClock(AnalogClock, 12,17);
+
+
+/* 인터페이스 확장(Extending Interfaces) 
+인터페이스를 재사용 가능한 컴포넌트로 분리하는 방법
+*/
+
+interface Shape{
+    color:string;
+}
+
+interface PenStroke {
+    penWidth: number;
+}
+interface Square extends Shape, PenStroke{
+    sideLength: number;
+}
+
+let square = <Square>{};
+square.color = "blue";
+square.sideLength = 10;
+square.penWidth = 5.0;
+
+
+/* 하이브리드 타입(Hybrid Types) */
+
+interface Counter{
+    (start:number):string;
+    interval:number;
+    reset():void;
+}
+
+function getCounter():Counter{
+    let counter = <Counter>function(start:number){};
+    counter.interval = 123;
+    counter.reset=function(){};
+
+    return counter;
+}
+//함수와 객체 역할을 모두 하는 객체
+let c = getCounter();
+c(10);
+c.reset();
+c.interval=5.0;
+
+
+/* 인터페이스 확장 클래스(Interfaces Extending classes) */
+
+class Control{
+    private state:any;
+}
+
+interface SelectableControl extends Control{
+    select(): void;
+}
+//state는 private 멤버이기 때문에 Control의 자식만 SelectableControl을 구현할 수 있다
+class Button extends Control implements SelectableControl{
+    select(){}
+}
+class TextBox extends Control{
+    select(){}
+}
+//Button과 TextBox 클래스는 SelectableControl의 하위 타입
+//오류 mage타입의 state프로퍼티가 없다. (왜냐하면 둘 다 Control을 상속받으며 select 메서드를 가지기 때문)
+// class mage implements SelectableControl{
+//     select(){}
+// }
+/* don't understand~ 
+인덱싱 가능 타입
+클래스의 스태틱과 인스턴스의 차이점
+하이블디ㅡ..
+그냥 뭐.. 다.. 쥬륵
+또 복습하기로이만
+뿅!
+참고 : https://typescript-kr.github.io/pages/Interfaces.html
+*/
