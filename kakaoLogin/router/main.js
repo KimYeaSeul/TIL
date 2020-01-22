3ec39f9a708b453a80648f5830d6b4cdcc62134b
KimYeaSeul <adgjl3151@hanmail.net>
2020-01-22 22:21:47 +0900

    add ejs, module

--- kakaoLogin/router/main.js
+++ kakaoLogin/router/main.js
@@ -1,9 +1,9 @@
-module.exports = function(app)
+module.exports = function(app, fs)  //fs모듈 추가
 {
   app.get('/',function(req,res){
-    res.render('index.html')
-  });
-  app.get('/about',function(req,res){
-    res.render('about.html');
+    res.render('index',{  //json데이터 페이지에 전달
+      title : "MyHome",
+      length : 5
+    })
   });
 }
--- kakaoLogin/server.js
+++ kakaoLogin/server.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var app = express();
-var router = require('./router/main')(app);
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var fs = require("fs");
 
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); //템플릿 엔진, 템플릿을 읽어 엔진의 문법과 설정에 따라서 파일을 HTML 형식으로 변환시키는 모듈
 app.engine('html', require('ejs').renderFile);
-app.use(express.static('public'));
 
 var server = app.listen(3000, function(){
-    console.log("Express server has started on port 3000")
+  console.log("Express server has started on port 3000")
 })
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(session({
+  secret : 'thisismysecret', //쿠키를 임의로 변조하는것을 방지하기 위한 sign 값 입니다. 원하는 값을 넣으면 됩니다.
+  resave : false, //세션을 언제나 저장할 지 (변경되지 않아도) 정하는 값입니다. express-session documentation에서는 이 값을 false 로 하는것을 권장하고 필요에 따라 true로 설정합니다.
+  saveUninitialized: true // 새로 생겼지만 변경되지 않은 세션을 의미합니다. Documentation에서 이 값을 true로 설정하는것을 권장합니다.
+}));
+
+var router = require('./router/main')(app,fs); //body-parser설정보다 뒤에 있어야 작동
