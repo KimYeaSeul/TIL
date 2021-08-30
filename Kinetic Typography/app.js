e16cec18f1a50a12284afd11ae4a0a7127378b4d
KimYeaSeul <adgjl3151@hanmail.net>
2021-08-30 14:42:41 +0900

    complete kinetic typography 210830

--- Kinetic Typography/app.js
+++ Kinetic Typography/app.js
@@ -11,12 +11,7 @@ class App {
             },
             // https://withthisclue.tistory.com/entry/Javascript-%ED%8E%98%EC%9D%B4%EC%A7%80-width-%EA%B5%AC%ED%95%98%EA%B8%B0-documentwidth-elementclientWidth-windowscreenwidth-%EC%B0%A8%EC%9D%B4
             fontactive: () => {
-
-                // this.text = new Text();
-                // this.text.setText('B', 2, document.body.clientWidth, document.body.clientHeight);
-                
                 this.visual = new Visual();
-
                 window.addEventListener('resize', this.resize.bind(this), false);
                 this.resize();
 
@@ -26,18 +21,55 @@ class App {
     }
     setWebgl(){
         this.renderer = new PIXI.Renderer({
-            width : document.body.clientWidth,
-            height : document.body.clientHeight,
+            width: document.body.clientWidth,
+            height: document.body.clientHeight,
             antialias : true,
             transparent : false,
-            resolution : (window.devicePixelRatio > 1) ? 2 :1,
+            resolution : (window.devicePixelRatio > 1) ? 2 : 1,
             autoDensity: true,
             powerPreference : "high-performance",
-            backgroundColor : 0xffffff,
+            // backgroundColor : 0xffffff,
+            backgroundColor : 0xff4338,
         });
         document.body.appendChild(this.renderer.view);
 
         this.stage = new PIXI.Container();
+
+        const blurFfilter = new PIXI.filters.BlurFilter();
+        blurFfilter.blur = 10;
+        blurFfilter.autoFit = true;
+
+        const fragSource = `
+        precision mediump float;
+        varying vec2 vTextureCoord;
+        uniform sampler2D uSampler;
+        uniform float threshold;
+        uniform float mr;
+        uniform float mg;
+        uniform float mb;
+        void main(void){
+            vec4 color = texture2D(uSampler, vTextureCoord);
+            vec3 mcolor = vec3(mr, mg, mb);
+            if(color.a > threshold) {
+                gl_FragColor = vec4(mcolor, 1.0);
+            }else{
+                gl_FragColor = vec4(vec3(0.0), 0.0);
+            }
+        }`;
+
+        const uniformsData = {
+            threshold: 0.5,
+            // mr: 0.0 / 255.0,
+            mr: 244.0 / 255.0,
+            // mg: 0.0 / 255.0,
+            mg: 193.0 / 255.0,
+            // mb: 0.0 / 255.0,
+            mb: 41.0 / 255.0,
+        };
+
+        const thresholdFilter = new PIXI.Filter(null, fragSource,uniformsData);
+        this.stage.filters = [blurFfilter, thresholdFilter];
+        this.stage.filterArea = this.renderer.screen;
     }
 
     resize(){
--- Kinetic Typography/particle.js
+++ Kinetic Typography/particle.js
@@ -1,11 +1,15 @@
-const FRICTION = 0.86;
-const MOVE_SPEED = 0.1;
+// const FRICTION = 0.86; // 마찰
+// const MOVE_SPEED = 0.1;
+
+// 위는 흰-검, 아래는 빨-노
+const FRICTION = 0.98;
+const MOVE_SPEED = 0.2;
 
 export class Particle{
     constructor(pos, texture){
         this.sprite = new PIXI.Sprite(texture);
         this.sprite.scale.set(0.2);
-        this.sprite.tint = 0x000000;
+        // this.sprite.tint = 0x000000;
 
         this.savedX = pos.x;
         this.savedY = pos.y;
@@ -13,14 +17,16 @@ export class Particle{
         this.y = pos.y;
         this.sprite.x = this.x;
         this.sprite.y = this.y;
-        this.vx = 0;
+        this.vx = 0; 
         this.vy = 0;
         this.radius = 10;
     }
 
     draw() {
-        this.vx += (this.savedX - this.x) * MOVE_SPEED;
-        this.vy += (this.savedY - this.y) * MOVE_SPEED;
+        // this.vx += (this.savedX - this.x) * MOVE_SPEED;
+        this.x += (this.savedX - this.x) * MOVE_SPEED;
+        // this.vy += (this.savedY - this.y) * MOVE_SPEED;
+        this.y += (this.savedY - this.y) * MOVE_SPEED;
 
         this.vx *=FRICTION;
         this.vy *=FRICTION;
